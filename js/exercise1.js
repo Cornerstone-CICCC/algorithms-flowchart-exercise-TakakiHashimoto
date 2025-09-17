8058fb906cb4285726a5375124a58f246e520667
gooko <takakihashimotogoo@gmail.com>
2025-09-16 20:22:50 -0700

    modifed variable names

--- js/exercise1.js
+++ js/exercise1.js
@@ -1,13 +1,18 @@
-import inquirer from "inquirer"
+// import inquirer from "inquirer"
 
-inquirer.prompt([
-    {
-        type: "input",
-        name: "LFT",
-        message: "Type in length in feet"
-    }
-]).then(answers => {
-    let LFT = answers.LFT;
-    let LCM = LFT * 30;
-    console.log(`${LFT} feet in cm is ${LCM} cm`);
-})
+// inquirer.prompt([
+//     {
+//         type: "input",
+//         name: "LFT",
+//         message: "Type in length in feet"
+//     }
+// ]).then(answers => {
+//     let LFT = answers.LFT;
+//     let LCM = LFT * 30;
+//     console.log(`${LFT} feet in cm is ${LCM} cm`);
+// })
+
+let LFT = 6
+let LCM = LFT * 30
+
+console.log(`${LFT} feet in cm is ${LCM} cm`)
--- js/exercise2.js
+++ js/exercise2.js
@@ -3,5 +3,5 @@ import inquirer from "inquirer"
 let width = 6
 let height = 5
 
-console.log(`Area is ${width * height}`)
+console.log(`The area is ${width * height}`)
 
